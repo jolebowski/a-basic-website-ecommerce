51ed4878e0506efef15f7ee692226ecbe829e461
jolebowski <jordan.moreira@epitech.eu>
2022-08-19 00:01:43 +0200

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-import Home from "./components/home/Home";
+import { Routes, Route, Outlet } from "react-router-dom";
+import HomePage from "./routes/homePage";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am navigation</h1>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+const Test = () => <h1>I am the best</h1>;
 
 const App = () => {
-  const catagories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  return <Home catagories={catagories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<HomePage />} />
+        <Route path="test" element={<Test />} />
+      </Route>
+    </Routes>
+  );
 };
-
 export default App;
